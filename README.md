417ba7cf45fb83fe23c5d106262233ae8b2b81bb
Eric Ford <ericford@mac.com>
2022-10-07 13:14:09 -0400

    Finished up PostgreSQL code. Started on final changes to readme.

--- README.md
+++ README.md
@@ -12,3 +12,50 @@
 1. Create several tables in PostgreSQL to store this information in a simple and relational manner.
 
 1. Conduct the analyses either directly in Python (and load the results to PostgreSQL), or load the raw data into PostgreSQL and write queries to analyze the data—determine and justfy the better choice.
+
+#### Implementation
+
+As the project was short it was implemented as a single Python module which can be run directly from the terminal, as such `python import.py`.
+
+#### Results
+
+1. The ten zipcodes with the highest levels of poverty are (n.b. several zip codes were listed as having either 100% or 0% poverty rates. I did not remove those zip codes.). The query and results are below.
+
+```sql
+SELECT
+      zip_stats.zipcode, 
+      poverty_level_rank, 
+      name 
+FROM zip_stats 
+     JOIN zip_raw_data ON zip_stats.zipcode = zip_raw_data.zipcode 
+     JOIN states ON zip_raw_data.state_id = states.id
+LIMIT 10;
+```
+
+
+|zipcode | rank | state | 
+|--------|:----:|-------|
+|38630   | 1    | Mississippi |
+|71659   | 1    | Arkansas |
+|95443   | 1    | California |
+|22548   | 1    | Virginia |
+|44503   | 1    | Ohio |
+|01009   | 1    | Massachusetts |
+|26348   | 1    | West Virginia |
+|41845   | 2    | Kentucky |
+|40997   | 3    | Kentucky |
+|62523   | 4    | Illinois |
+
+#### Choices and justifications
+
+1. Extra data was collected from the census in order for more complex analysis. Specifically, for each zipcode: 
+    1. The total population
+    1. Gross rent as a percentage of household income
+    1. Median household income (dollars)
+    1. Margin of error in poverty level: not currently collecting  
+
+    For instance, by comparing the median household income against the current poverty rate ($26,500 for a family of four in 2020) or against the gross rent as a percentage of household, the relative strength of zip code-level poverty might be better indicated.
+
+1. Rather than do any of the analyses in Python, I chose to do all the computation in the database. This was for two reasons: first, my expectation is that a data scientist, data analyst, or BI would be the final consumer of the data and they would interact directly with the database; second, as the world moves more and more to data warehouses and databases grow in size, doing intensive computation locally will not scale as well as doing the computation directly in the warehouse.
+
+
--- import.py
+++ import.py
@@ -13,25 +13,19 @@ PSQL_DATABASE_NAME = getenv("PSQL_DATABASE_NAME")
 
 c = Census(API_KEY, year=2019)  # 2020 returns nulls for the state.
 
-
-# API_CALL = (
-#     "https://api.census.gov/data/2020/acs/acs5/profile?get=NAME,DP03_0119PE"
-#     f"&for=zip%20code%20tabulation%20area:*&key={API_KEY}"
-# )
-
 # Collecting info on these variables
 # DP05_0001PE Total population
 # DP04_0136PE Gross rent as a percentage of household income
 # DP03_0062E Median household income (dollars)
 # DP03_0119PE Percentage of families and people whose income in the past 12
 #             months is below the poverty level
+# DP03_0119PM -- Margin of error in poverty level: not currently collecting
 
-# Note that for data
-# # profile tables must use Census.acs5db() and not Census.acs5().
-table = c.acs5dp.get(
-    ("DP05_0001PE", "DP04_0136PE", "DP03_0062E", "DP03_0119PE"),
-    {"for": "zip code tabulation area:*"},
-)
+# Note that for data profile tables we must use Census.acs5db() and not Census.acs5().
+# table = c.acs5dp.get(
+#     ("DP05_0001PE", "DP04_0136PE", "DP03_0062E", "DP03_0119PE"),
+#     {"for": "zip code tabulation area:*"},
+# )
 # print(table)
 # for i, row in enumerate(table):
 #     print(i, row["DP03_0119PE"], row["state"], row["zip code tabulation area"])
@@ -42,68 +36,131 @@ table = c.acs5dp.get(
 with open(
     (
         "/Users/eric/Documents/workspace_git/census-county-level-poverty-project/"
-        "state codes.csv"
+        "state_codes.csv"
     ),
     "r",
 ) as states_in:
     states = csv.DictReader(states_in, delimiter=",", quotechar='"')
-    state_lookup = {s["value"]: s["name"] for s in states}
-# print(state_lookup)
+    states_to_insert = [(s["value"], s["name"], s["abbreviation"]) for s in states]
+
+
+# with open(
+#     "/Users/eric/Documents/workspace_git/census-county-level-poverty-project/zips.csv",
+#     "w",
+# ) as zips:
+#     zip_writer = csv.writer(
+#         zips, delimiter=",", quotechar='"', quoting=csv.QUOTE_MINIMAL
+#     )
+#     zip_writer.writerow(
+#         [
+#             "Total population",
+#             "Gross rent as a percentage of household income",
+#             "Median household income (dollars)",
+#             "Percent below poverty line",
+#             "STATE",
+#             "zipcode",
+#         ]
+#     )
+#     for row in table:
+#         if (
+#             row["DP05_0001PE"] != "0.0"
+#             and row["DP03_0119PE"] >= 0
+#             and row["DP03_0062E"] >= 0
+#         ):
+#             # Throwing out bad data, which comes back as negative numbers.
+#             # Consider margin of error here? Some zips return 0% or 100%
+#             # poverty levels. Including those for now.
+#             zip_writer.writerow(
+#                 [
+#                     row["DP05_0001PE"],
+#                     row["DP04_0136PE"],
+#                     row["DP03_0062E"],
+#                     row["DP03_0119PE"],
+#                     row["state"],
+#                     row["zip code tabulation area"],
+#                 ]
+#             )
 
 with open(
-    "/Users/eric/Documents/workspace_git/census-county-level-poverty-project/zips.csv",
-    "w",
-) as zips:
-    zip_writer = csv.writer(
-        zips, delimiter=",", quotechar="|", quoting=csv.QUOTE_MINIMAL
-    )
-    zip_writer.writerow(
-        [
-            "Total population",
-            "Gross rent as a percentage of household income",
-            "Median household income (dollars)",
-            "Percent below poverty line",
-            "STATE",
-            "zipcode",
-        ]
-    )
-    for row in table:
-        if (
-            row["DP05_0001PE"] != "0.0"
-            and row["DP03_0119PE"] != -666666666.0
-            and row["DP03_0062E"] != -666666666.0
-        ):
-            zip_writer.writerow(
-                [
-                    row["DP05_0001PE"],
-                    row["DP04_0136PE"],
-                    row["DP03_0062E"],
-                    row["DP03_0119PE"],
-                    state_lookup[row["state"]],
-                    row["zip code tabulation area"],
-                ]
-            )
+    (
+        "/Users/eric/Documents/workspace_git/census-county-level-poverty-project/"
+        "zips.csv"
+    ),
+    "r",
+) as zips_in:
+    zips = csv.DictReader(zips_in, delimiter=",", quotechar='"')
+    zips_to_insert = [
+        (
+            z["zipcode"],
+            int(float(z["Total population"])),
+            z["Gross rent as a percentage of household income"],
+            int(float(z["Median household income (dollars)"])),
+            z["Percent below poverty line"],
+            z["STATE"],
+        )
+        for z in zips
+    ]
 
-# with psycopg.connect(f"dbname={PSQL_DATABASE_NAME} user=postgres password={PSQL_PW}") as conn:
-#     # Open a cursor to perform database operations
-#     with conn.cursor() as cur:
-#         # Execute a command: this creates a new table
-#         cur.execute(
-#             "DROP TABLE IF EXISTS poverty_zips"
-#         )
-#         cur.execute(
-#             """
-#             CREATE TABLE poverty_zips (
-#                 id serial PRIMARY KEY,
-#                 poverty_level float,
-#                 zipcode integer UNIQUE,
-#                 decile integer,
-#                 state varchar(15)
-#             )
-#             PARTITION BY RANGE (decile)
-#             """
-#         )
-#         # cur.execute("CREATE INDEX zip_idx ON poverty_zips (zipcode)")
+
+with psycopg.connect(
+    f"dbname={PSQL_DATABASE_NAME} user=postgres password={PSQL_PW}"
+) as conn:
+    # Open a cursor to perform database operations
+    with conn.cursor() as cur:
+        # Create state_ids table to translate from numeric IDs returned from
+        # census to text. Both state name and postal code are stored.
+        cur.execute("DROP TABLE IF EXISTS states CASCADE")
+        cur.execute(
+            """
+            CREATE TABLE states (
+                id INT PRIMARY KEY,
+                name varchar(50),
+                abbreviation varchar(2)
+            )
+            """
+        )
+        insert_sql = "INSERT INTO states (id, name, abbreviation)" "VALUES (%s, %s, %s)"
+        cur.executemany(insert_sql, states_to_insert)
+
+        # Now create table of raw data.
+        cur.execute("DROP TABLE IF EXISTS zip_raw_data CASCADE")
+        cur.execute(
+            """
+            CREATE TABLE zip_raw_data (
+                zipcode varchar(5) PRIMARY KEY,
+                population INT,
+                rent_as_pctg float,
+                median_income INT,
+                poverty_level float,
+                state_id INT REFERENCES states (id)
+            )
+            """
+        )
+        insert_sql = (
+            "INSERT INTO zip_raw_data (zipcode, population, "
+            "rent_as_pctg, median_income, poverty_level, state_id)"
+            "VALUES (%s, %s, %s, %s, %s, %s)"
+        )
+        cur.executemany(insert_sql, zips_to_insert)
+
+        # Finally, create table with various stats.
+        # Currently, this breaks
+        cur.execute("DROP TABLE IF EXISTS zip_stats")
+        cur.execute(
+            """
+            CREATE TABLE zip_stats
+            AS
+            SELECT
+                zipcode,
+                NTILE(5) OVER (ORDER BY poverty_level DESC) AS quintiles,
+                DENSE_RANK() OVER (
+                    ORDER BY poverty_level DESC
+                ) poverty_level_rank
+            FROM zip_raw_data
+            """
+        )
+
+# poverty level family of four: 26500
 
 
 # by state: proportion per state
